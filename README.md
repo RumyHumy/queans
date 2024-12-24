2e74e0286deb6da163f1d913082e97d080ebdfcd
Ramil Khasanshin <khasanshin.ramil@list.ru>
2024-12-24 23:45:54 +0300

    Breaks everything

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Linux Python script for simple "term-defenition" text quizes
 
 # FILE LAYOUT
 ```
+QUESTION 1:
 question??answer
 
+QUESTION 2:
 >>>
 multiline
 question
@@ -15,15 +17,15 @@ answer
 ```
 
 
-# OPTIONS:
-```bash
-./termdef.py <file1> <file2> ... <filen> [OPTIONS]
---term-def(inition)/--td - asks you term
---def(inition)-term/--dt - asks you defenition
---asks-random/--ar - asks at random
---single - ask single question
---all    - ask all of the avaliable entries
---count <number-of-questions>
---shuffle, --no-shuffle
---blur <radius-of-blur>
+# OPTIONS
+```text
+./termdef.py [OPTIONS] <file1> <file2> ... <filen>
+-term-def(inition) / -td - asks you term
+-def(inition)-term / -dt - asks you defenition
+-asks-random / -ar - asks at random term-def/def-term
+-single - ask single question
+-all    - ask all of the avaliable entries
+-count <number-of-questions>
+-shuffle, -no-shuffle
+-blur <radius> - shuffles nearest in <radius>
 ```
--- termdef.py
+++ termdef.py
@@ -8,6 +8,7 @@ class State:
     order = 'td' # td (term-def), dt (def-term), ar (asks-random)
     count = None # All
     blur  = None # Complete randomness
+    use_score = True
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -27,34 +28,38 @@ if __name__ == '__main__':
     i = 1
     while i < len(sys.argv):
         arg = sys.argv[i]
-        if   arg in ['--term-def', '--term-definition', '--td']:
+        if   arg in ['-term-def', '-term-definition', '-td']:
             State.order = 'td'
-        elif arg in ['--def-term', '--definition-term', '--dt']:
+        elif arg in ['-def-term', '-definition-term', '-dt']:
             State.order = 'dt'
-        elif arg in ['--asks-random', '--ar']:
+        elif arg in ['-asks-random', '-ar']:
             State.order = 'ar'
-        elif arg in ['--single']:
+        elif arg in ['-single']:
             State.count = 1
-        elif arg in ['--all']:
+        elif arg in ['-all']:
             State.count = None
-        elif arg in ['--shuffle']:
+        elif arg in ['-shuffle']:
             State.blur = None
-        elif arg in ['--no-shuffle']:
+        elif arg in ['-no-shuffle']:
             State.blur = 0
-        elif arg in ['--count']:
+        elif arg in ['-count']:
             i += 1
             narg = subarg(i, arg)
             if not narg.isnumeric():
                 wrong_subarg(arg)
             State.count = int(narg)
-        elif arg in ['--blur']:
+        elif arg in ['-blur']:
             i += 1
             narg = subarg(i, arg)
             if not narg.isnumeric():
                 wrong_subarg(arg)
             State.blur = int(narg)
-        elif arg[:2] == '--':
-            print('Unknown "--"-argument')
+        elif arg in ['-score']:
+            State.use_score = None
+        elif arg in ['-no-score']:
+            State.use_score = 0
+        elif arg[0] == '-':
+            print('Unknown "-"-argument')
             exit(1)
         else:
             State.fnames.append(arg)
@@ -69,62 +74,10 @@ if __name__ == '__main__':
     class Card:
         content = None
         fname = None
-        line = None
-    cards = []
-    entry_flag = False
-    for fname in State.fnames:
-        file = open(fname, 'r', encoding='utf-8')
-        lines = file.readlines()
-        file.close()
-        card = None
-        previ = None
-        for i, l in enumerate(lines):
-            if entry_flag == False:
-                if not l.find('??') in [-1, 0]:
-                    card = Card()
-                    card.content = l.split('??', 1)
-                    card.fname = fname
-                    card.line = i
-                    cards.append(card)
-            if l == '>>>\n':
-                if entry_flag == True:
-                    print(f'Unexpected ">>>" in "{fname}:{i}"')
-                    print('Ignoring file.')
-                    break
-                entry_flag = True
-                card = Card()
-                card.content = []
-                card.line = i
-                card.fname = fname
-                previ = i
-            elif l == '??\n':
-                if entry_flag == False:
-                    print(f'Unexpected "??" in "{fname}:{i}"')
-                    print('Ignoring file.')
-                    break
-                card.content.append(''.join(lines[previ+1:i]))
-                previ = i
-            elif l == '<<<\n':
-                if entry_flag == False:
-                    print(f'Unexpected "<<<" in "{fname}:{i}"')
-                    print('Ignoring file.')
-                    break
-                if len(card.content) == 0:
-                    print(f'Unexpected end of entry in "{fname}:{i}"')
-                    print(f'("??" expected)')
-                    print('Ignoring file.')
-                    break
-                entry_flag = False
-                card.content.append(''.join(lines[previ+1:i]))
-                cards.append(card)
-                print(card)
-        if entry_flag == True:
-            print(f'Unexpected EOF in "{fname}"')
-            print('Ignoring file.')
-            break
-
-    if State.count == None:
-        State.count = len(cards)
+        bline = None
+        eline = None
+        score = None
+    exit()
 
     # SHUFFLE
     #
@@ -137,18 +90,27 @@ if __name__ == '__main__':
             p2 = max(0, min(p2, len(cards)-1))
         cards[p1], cards[p2] = cards[p2], cards[p1]
 
+    # SORT BY SCORE
+    #
+    #cards.sort(key=lambda x: x.score, reverse=True)
+
     # QUIZ
     #
     print('Les go...')
     inv = State.order == 'dt'
     for i, card in enumerate(cards):
+        print(card.content, card.score)
         if i > State.count-1:
             break
         if State.order == 'ar':
             inv = randint(0, 1)
         print(card.content[inv], end='')
-        input()
+        input('...')
         print(card.content[1-inv], end='')
-        score = input('Good/Weak/Bad (g/w/B): ').lower()
-        score = 2 if score == 'g' else 1 if score == 'w' else 0
+        if State.use_score == True:
+            if card.score == None:
+                card.score = 0
+            score = input('Good/Weak/Bad (g/w/B): ').lower()
+            score = 1 if score == 'g' else 0 if score == 'w' else -1
+            card.score = min(0, card.score+score)
         print()
